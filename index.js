2939284570726c5ab41c178a974d3e3522a41751
Alyson Takai <shtakai@gmail.com>
2016-07-08 15:31:27 -0700

    fix

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
-//app.use(express.static(path.join(__dirname, './static')));
+app.use(express.static(path.join(__dirname, './static')));
 
 app.get('/', function(req, res){
   survey = null;
