d146df0dab3ce4c740da8c6cc9a0fe6234e5a10e
Alyson Takai <shtakai@gmail.com>
2016-07-08 12:52:06 -0700

    remove static

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-app.use(express.static(`${__dirname}/static`));
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, './static')));
+//app.use(express.static(path.join(__dirname, './static')));
 
 app.get('/', function(req, res){
   console.log('index');
