8d813b4bb2201e905f1c183a91566787b13233e1
Alyson Takai <shtakai@gmail.com>
2016-07-08 13:48:45 -0700

    form

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var port;
+var port,
+  survey=null;
 port = 8000;
 var express = require('express');
 var path = require('path');
@@ -14,19 +15,26 @@ app.set('view engine', 'ejs');
 //app.use(express.static(path.join(__dirname, './static')));
 
 app.get('/', function(req, res){
+  survey = null;
   console.log('index');
   res.render('index');
 });
 
 app.post('/survey', function(req, res){
+
+  // NOTE: without validation
   console.log('survey');
+  console.log(`Post Data\n\n${JSON.stringify(req.body)}`);
+  survey = req.body
   res.redirect('/result');
 });
 
 app.get('/result', function(req, res){
   console.log('result');
-  res.send('result');
-})
+  survey = survey || {};
+  console.log(`survey:${JSON.stringify(survey)}`);
+  res.render('result', { survey: survey });
+});
 
 
 app.listen(port, function(){
